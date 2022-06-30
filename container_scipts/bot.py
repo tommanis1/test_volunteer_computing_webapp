57df0d453ad62d7a38826f81d5b95411b1a0182e
Tommaso Trooster <trooster.t@gmail.com>
2022-06-30 18:10:33 +0200

    update

--- container_scipts/bot.py
+++ container_scipts/bot.py
@@ -2,19 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
 import time
-PROJECT = 1
+import sys
+PROJECT = 3
 class Bot():
-    def __init__(self):
+    def __init__(self, id):
         self.driver = webdriver.Firefox(executable_path=os.path.join(os.getcwd(), "geckodriver"))
-        self.id = 1
-        # self.id = os.environ['TEST_ID']
+        self.id = id
         self.project = PROJECT
-        self.base_url = "http://localhost:3601/"
+        self.base_url = "http://146.190.25.75:3601"
     def log_in(self):
-    #     # t = self.driver.get("http://146.190.25.75:3601/login")
         self.driver.get(self.base_url + "login")
     #     # fill user and password
-
         self.driver.find_element(By.ID, "uname-normal").send_keys(f"test_account_id_{self.id}")
         self.driver.find_element(By.ID, "password-normal").send_keys(f"test_account_id_{self.id}")
         self.driver.find_element(By.XPATH, '//button[text()="Log in"]').click()
@@ -32,11 +30,12 @@ class Bot():
     def run_project(self):
         self.driver.get(self.base_url + f"api/runproject/{self.project}")
 
-        # "//*[text()[contains(., 'Welcome to the admin page!')]]
-        # class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiAvatar-fallback css-13y7ul3"
+def main():
+    b = Bot(sys.argv[1])
+    b.sign_up()
+    b.log_in()
+    time.sleep(1)
+    b.run_project()
 
-b = Bot()
-b.sign_up()
-b.log_in()
-time.sleep(1)
-b.run_project()
+if __name__ == "__main__":
+    main()
